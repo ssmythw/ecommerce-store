53acf4422ae6324de8e70a6ee9c7601622975369
Scott <ssmythw@outlook.com>
2023-06-14 12:00:52 -0400

    added images to backend through cloudinary

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const cors = require("cors");
+const products = require("./products");
 
 const app = express();
 
-app.use(express.json);
-app.use(cors);
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Welcome to the API");
+  res.end();
 });
 
-app.listen(5000, console.log("Server running on port 5000"));
+app.get("/products", (req, res) => {
+  res.send(products);
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`));
---
+++ backend/products.js
@@ -0,0 +1,28 @@
+const products = [
+  {
+    id: 1,
+    name: "iPhone 12 Pro",
+    desc: "6.1 inch display",
+    price: 999.99,
+    image:
+      "https://res.cloudinary.com/dmlxrsrox/image/upload/v1686758355/ecommerce-shop/cnwlmrbxqt5rsmgy0ert.jpg",
+  },
+  {
+    id: 2,
+    name: "iPhone 12",
+    desc: "5,4 inch display",
+    price: 699.99,
+    image:
+      "https://res.cloudinary.com/dmlxrsrox/image/upload/v1686758355/ecommerce-shop/gww4btmgzlr8isxqbx1x.jpg",
+  },
+  {
+    id: 3,
+    name: "Galaxy S",
+    desc: "6.5 inch display",
+    price: 399.99,
+    image:
+      "https://res.cloudinary.com/dmlxrsrox/image/upload/v1686758355/ecommerce-shop/q0uzpazw8lg4i3z6y4i5.png",
+  },
+];
+
+module.exports = products;
