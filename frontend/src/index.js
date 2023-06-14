f5c0b7a8575338d58f716cce9bebcc913d59a891
Scott <ssmythw@outlook.com>
2023-06-14 12:15:35 -0400

    added redux toolkit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import productReducer from "./features/productSlice";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = configureStore({
+  reducer: {
+    productReducer,
+  },
+});
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
