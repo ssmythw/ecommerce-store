d2826bbbdd95a36f5644ac7e38cadff557928b48
Scott <ssmythw@outlook.com>
2023-06-15 12:05:36 -0400

    added async thunk to reducer

--- frontend/src/features/productSlice.js
+++ frontend/src/features/productSlice.js
@@ -1,14 +1,30 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   items: [],
   status: null,
 };
 
+// action type
+// 3 get generated: products/productsFetch/pending, /fulfilled, /rejected
+// actual function to get data
+
+export const productsFetch = createAsyncThunk(
+  "products/productsFetch",
+  async () => {
+    const res = await axios.get("http://localhost:5000/products");
+    return res?.data;
+  }
+);
+
 const productSlice = createSlice({
   name: "products",
   initialState,
   reducers: {},
+  extraReducers: {
+    [productsFetch.pending]
+  }
 });
 
 export default productSlice.reducer;
