edae3b134e7e3006c25b68875ba802f9c43632ec
Scott <ssmythw@outlook.com>
2023-06-16 19:04:03 -0400

    added front end logic to display products

---
+++ frontend/src/features/cartSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  cartTotalQuantity: 0,
+  cartTotalAmount: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    
+  },
+});
