74dd4901a91b9b53ae52224ded921ca115304403
Scott <ssmythw@outlook.com>
2023-06-16 11:22:26 -0400

    added frontend logic to retrieve backend data

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,34 @@
 import React from "react";
-import { useGetAllProductsQuery } from "../features/productsAPI";
+import { productAPI, useGetAllProductsQuery } from "../features/productsAPI";
 
 const Home = () => {
   const { data, error, isLoading } = useGetAllProductsQuery();
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An error occured</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {data?.map((product) => (
+              <div className="product" key={product.id}>
+                <h3>{product.name}</h3>
+                <img src={product.image} alt="" />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span className="price">{product.price}</span>
+                </div>
+                <button>Add To Cart</button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
