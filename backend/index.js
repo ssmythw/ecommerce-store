dd4696b0cee77a3d62f632aa9559b1132e1fcad9
Scott <ssmythw@outlook.com>
2023-06-18 13:54:43 -0400

    set up mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const products = require("./products");
 
 const app = express();
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
@@ -17,5 +19,14 @@ app.get("/products", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
+const URI = process.env.DB_URI;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
+
+mongoose
+  .connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to db"))
+  .catch((err) => console.log("DB connection failed", err.message));
