0607e3a4900e20982cc5f4236bfa2fa7ab614050
Scott <ssmythw@outlook.com>
2023-06-18 14:24:22 -0400

    added jwt

---
+++ backend/models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  email: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 200,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 3,
+    maxLength: 1024,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
